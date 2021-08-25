838f99dfa630eee9d797ad76f653acc82646e545
jayesh200026 <jayeshkumar8660@gmail.com>
2021-08-25 22:13:50 +0530

    Main class added

---
+++ Day9Programs/src/com/Main/Main.java
@@ -0,0 +1,33 @@
+package com.Main;
+
+import com.Logical.Fibonacci;
+import com.Logical.PerfectNumber;
+import com.Logical.PrimeNumber;
+import com.Logical.ReverseNumber;
+import com.Logical.StopWatch;
+
+import JUnit.Binary;
+import JUnit.DayOfWeek;
+import JUnit.MonthlyPayment;
+import JUnit.TempratureConversion;
+import JUnit.ToBinary;
+import JUnit.VendingMachine;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Fibonacci.fibonacci();
+		//PerfectNumber.perfectNumber();
+		//PrimeNumber.primeNumber();
+		//ReverseNumber.reverseNumber();
+		//StopWatch.stopWatch();
+		//VendingMachine.vendingMachine();
+		//DayOfWeek.dayOfWeek(args);
+		//TempratureConversion.tempConversion();
+		//MonthlyPayment .monthpay(args);
+		//ToBinary.toBinary();
+		Binary.nibble();
+
+	}
+
+}
